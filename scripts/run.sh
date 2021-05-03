e44753244b31aac071393808d3c607aa9a5702d8
AaronAnima <1700012896@pku.edu.cn>
2021-05-04 01:33:08 +0800

    removed some dependency on tensorflow added a script for testing removed the launcher, it is simpler to launch the algorithm directly

---
+++ scripts/run.sh
@@ -0,0 +1,9 @@
+env=CartPole-v1
+algorithm=ppo
+name=${env}_${algorithm}
+gpu=0
+seed=0
+
+CUDA_VISIBLE_DEVICES=$gpu python -m spinup.algos.pytorch.$algorithm.$algorithm  --exp_name $name --env $env  
+    --hid [32,32] [64,32]  --seed $seed  --cpu auto
+    
